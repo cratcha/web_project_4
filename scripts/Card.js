5879f47c359c4de6c0118a80efb8265232db322d
Ekaterina Cratcha <ekaterina.cratcha@yandex.com>
2022-01-09 23:18:12 -0500

    refactor: add setEventListeners method

--- scripts/Card.js
+++ scripts/Card.js
@@ -6,36 +6,52 @@ export class Card {
     this._template = template;
     this._data = data;
   }
+
   createCard() {
     this._card = this._template.content
       .querySelector(".element")
       .cloneNode(true);
-    const imageElement = this._card.querySelector(".element__photo");
+
     const titleElement = this._card.querySelector(".element__title");
-
-    const trashButton = this._card.querySelector(".element__trash");
-    const likeButton = this._card.querySelector(".element__like-button");
-
+    const imageElement = this._card.querySelector(".element__photo");
     imageElement.src = this._data.url;
-    titleElement.textContent = this._data.title;
     imageElement.alt = this._data.title;
+    titleElement.textContent = this._data.title;
 
-    imageElement.addEventListener("click", () => {
-      modalImageElement.src = this._data.url;
-      modalCaption.textContent = this._data.title;
-      modalImageElement.alt = this._data.title;
-      openModal(imageModal);
-    });
-
-    trashButton.addEventListener("click", () => {
-      const cardToDelete = trashButton.closest(".element");
-      cardToDelete.remove();
-    });
-
-    likeButton.addEventListener("click", () => {
-      likeButton.classList.toggle("element__like-button_activated");
-    });
+    this._setEventListeners();
 
     return this._card;
+  }
+
+  _setEventListeners() {
+    const imageElement = this._card.querySelector(".element__photo");
+    imageElement.addEventListener("click", (evt) => {
+      this._handlePreviewPicture();
+    });
+
+    this._likeButton = this._card.querySelector(".element__like-button");
+    this._likeButton.addEventListener("click", (evt) => {
+      this._handleLikeClick();
+    });
+    this._trashButton = this._card.querySelector(".element__trash");
+    this._trashButton.addEventListener("click", (evt) => {
+      this._handleTrashButtonClick();
+    });
+  }
+
+  _handlePreviewPicture() {
+    modalImageElement.src = this._data.url;
+    modalCaption.textContent = this._data.title;
+    modalImageElement.alt = this._data.title;
+    openModal(imageModal);
+  }
+
+  _handleLikeClick() {
+    this._likeButton.classList.toggle("element__like-button_activated");
+  }
+
+  _handleTrashButtonClick() {
+    const cardToDelete = this._trashButton.closest(".element");
+    cardToDelete.remove();
   }
 }
