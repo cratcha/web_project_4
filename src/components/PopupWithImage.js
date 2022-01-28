42555204e319e10aa997fd81f4964b1e0b33b09f
Ekaterina Cratcha <ekaterina.cratcha@yandex.com>
2022-01-27 23:35:07 -0500

    fix imagePopup

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -3,8 +3,8 @@ import Popup from "./Popup.js";
 class PopupWithImage extends Popup {
   constructor(popupSelector) {
     super(popupSelector);
-    this._imageElement = this._modal.querySelector(".modal__caption");
-    this._imageCaption = this._modal.querySelector(".modal__image");
+    this._imageElement = this._modal.querySelector(".modal__image");
+    this._imageCaption = this._modal.querySelector(".modal__caption");
   }
 
   openModal(data) {
